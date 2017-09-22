c920f7f791b70e876497d4c8b38a7e88a6f73fbe
David Arias <davidariasdeveloper@gmail.com>
2017-09-22 02:15:20 -0500

    Double pointer to single pointer

--- multiply_cuda.cu
+++ multiply_cuda.cu
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void fill_matrix(int **m,char c){
+void fill_matrix(int *m,char c){
 	cout<<"Llenamos matriz "<<endl;
 	for(int i=0;i<N;i++){
 		for(int j=0;j<N;j++){
@@ -21,21 +21,8 @@ void fill_matrix(int **m,char c){
 	}
 	return;
 }
-//Reservar memoria para la matriz
-void allocMatrix(int rows,int columns,int **matrix){
-  for(int i = 0; i < rows; i++)
-    matrix[i] = (int *)malloc(columns*sizeof(int));
-}
-//Liberar memoria de la matriz
-void freeMatrix(int rows,int **matrix){
-  int row;
-  for(row = 0;row<rows;row++){
-    free(matrix[row]);
-  }
-  free(matrix);
-}
 //Multiplicacion en CPU
-int multiply_seq(int **m1,int **m2,int **m3){
+int multiply_seq(int *m1,int *m2,int *m3){
 	cout<<"Multiplicamos con el algoritmo secuencial: \n"<<endl;
 	for(int i=0;i<N;i++){
 		for(int j=0;j<N;j++){
@@ -61,7 +48,7 @@ __global__ void multiply_par(int *a, int *b, int *c) {
 }
 
 //Imprimir matrices
-void print_matrix(int **m){
+void print_matrix(int *m){
 	for(int i=0;i<N;i++){
 		for(int j=0;j<N;j++){
 			cout<<"["<< m[i][j] <<"]";
@@ -74,25 +61,21 @@ void print_matrix(int **m){
 
 int main(){
 	//Declaracion de variables
-	int **matrixA = (int **)malloc(N*sizeof(int *));;
-	int **matrixB = (int **)malloc(N*sizeof(int *));;
-	int **matrixC = (int **)malloc(N*sizeof(int *));;
-
-	allocMatrix(N,N,matrixA);
-	allocMatrix(N,N,matrixB);
-	allocMatrix(N,N,matrixC);
+	int *h_A = (int *)malloc(N*N*sizeof(int *));;
+	int *h_B = (int *)malloc(N*N*sizeof(int *));;
+	int *h_C = (int *)malloc(N*N*sizeof(int *));;
 
 	clock_t t_i,t_f;
 	float tiempo;
 
-	fill_matrix(matrixA,'s');
-	fill_matrix(matrixB,'s');
-	fill_matrix(matrixC,'z');
-	//print_matrix(matrixA);
-	//print_matrix(matrixB);
+	fill_matrix(h_A,'s');
+	fill_matrix(h_B,'s');
+	fill_matrix(h_C,'z');
+	//print_matrix(h_A);
+	//print_matrix(h_B);
 
 	t_i=clock();
-	multiply_seq(matrixA,matrixB,matrixC);
+	multiply_seq(h_A,h_B,h_C);
 	t_f=clock();
 
 	tiempo= ((double)t_f - t_i) / CLOCKS_PER_SEC;
@@ -102,16 +85,16 @@ int main(){
 	cout<<"Multiplicamos con el algoritmo paralelo: \n"<<endl;
 
 	//Punteros de device
-	int **d_A,**d_B,**d_C;
+	int *d_A,*d_B,*d_C;
 
 	int size = N * N * sizeof(int);
 
-	cudaMalloc((void **) &d_A, size);//Reservar memoria en GPU
- 	cudaMalloc((void **) &d_B, size);
- 	cudaMalloc((void **) &d_C, size);
+	cudaMalloc((void *) &d_A, size);//Reservar memoria en GPU
+ 	cudaMalloc((void *) &d_B, size);
+ 	cudaMalloc((void *) &d_C, size);
 
- 	cudaMemcpy(d_A, matrixA, size, cudaMemcpyHostToDevice);//Pasar datos de CPU a GPU
- 	cudaMemcpy(d_B, matrixB, size, cudaMemcpyHostToDevice);
+ 	cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);//Pasar datos de CPU a GPU
+ 	cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
 
 	//Invocamos el kernel
 	dim3 dimBlock(N*N,2);
@@ -124,17 +107,17 @@ int main(){
 	cout<<"El tiempo de trabajo con el algoritmo en paralelo es de ";
 	printf("%f\n\n",tiempo);
 
-	cudaMemcpy(matrixC, d_C, size, cudaMemcpyDeviceToHost);//Pasar datos de GPU a CPU
+	cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);//Pasar datos de GPU a CPU
 
-	//print_matrix(matrixC);
+	//print_matrix(h_C);
 
   cudaFree(d_A);//Liberar memoria en GPU
  	cudaFree(d_B);
  	cudaFree(d_C);
 
-	freeMatrix(N,matrixA);
-	freeMatrix(N,matrixB);
-	freeMatrix(N,matrixC);
+	free(h_A);
+	free(h_B);
+	free(h_C);
 
 	return 0;
 }
